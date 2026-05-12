91e5f88afe80f487db09f81c596323801eb7a7d7
davidCavalcantee <david.cavalcante@sptech.school>
2026-05-11 22:56:03 -0300

    Adicionando o quiz com a inserção no Banco funcionando além de mudanças no css

---
+++ src/controllers/quizController.js
@@ -0,0 +1,98 @@
+ var quizModel = require("../models/quizModel");
+// // var aquarioModel = require("../models/aquarioModel");
+
+// function autenticar(req, res) {
+//     var email = req.body.emailServer;
+//     var senha = req.body.senhaServer;
+
+//     if (email == undefined) {
+//         res.status(400).send("Seu email está undefined!");
+//     } else if (senha == undefined) {
+//         res.status(400).send("Sua senha está indefinida!");
+//     } else {
+
+//         usuarioModel.autenticar(email, senha)
+//             .then(
+//                 function (resultadoAutenticar) {
+//                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+//                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+//                     if (resultadoAutenticar.length == 1) {
+//                         console.log(resultadoAutenticar);
+//                         // res.status(200).send()
+//                         res.json({
+//                                          id: resultadoAutenticar[0].id,
+//                                          email: resultadoAutenticar[0].email,
+//                                          nome: resultadoAutenticar[0].nome,
+//                                          senha: resultadoAutenticar[0].senha,
+//                                      });
+
+//                         //  usuarioModel(resultadoAutenticar[0])
+//                         //     .then(() => {
+                               
+//                         //             res.json({
+//                         //                 id: resultadoAutenticar[0].id,
+//                         //                 email: resultadoAutenticar[0].email,
+//                         //                 nome: resultadoAutenticar[0].nome,
+//                         //                 senha: resultadoAutenticar[0].senha,
+//                         //             });
+                                
+//                         //     })
+//                     } else if (resultadoAutenticar.length == 0) {
+//                         res.status(403).send("Email e/ou senha inválido(s)");
+//                     } else {
+//                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+//                     }
+//                 }
+//             ).catch(
+//                 function (erro) {
+//                     console.log(erro);
+//                     console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+//                     res.status(500).json(erro.sqlMessage);
+//                 }
+//             );
+//     }
+
+// }
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var acertos = req.body.acertosServer;
+    var fkUsuario = req.body.fkUsuarioServer;
+    // var senha = req.body.senhaServer;
+    // var fkEmpresa = req.body.idEmpresaVincularServer;
+
+    // Faça as validações dos valores
+    if (acertos == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (fkUsuario == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    // } else if (senha == undefined) {
+    //     res.status(400).send("Sua senha está undefined!");
+    // } else if (fkEmpresa == undefined) {
+    //     res.status(400).send("Sua empresa a vincular está undefined!");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        quizModel.cadastrar(acertos, fkUsuario)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao Inserir os dados do Quiz! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    // autenticar,
+    cadastrar
+}
---
+++ src/models/quizModel.js
@@ -0,0 +1,19 @@
+var database = require("../database/config")
+
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(acertos, fkUsuario) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", acertos, fkUsuario);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO quiz (acertos,fkUsuario) VALUES ('${acertos}', '${fkUsuario}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    cadastrar
+};
